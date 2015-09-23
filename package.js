6966ffbfd787471e0c5d5b944e3888c5a4381c54
Bugra Ozkan <bugra@stratagem.co>
2015-09-23 15:00:47 +0100

    b

--- package.js
+++ package.js
@@ -5,7 +5,7 @@
 Package.describe({
     "name": 'nvd3-sbet',
     summary: 'Nvd3.org charts.',
-    version: '0.0.1',
+    version: '1.0.0',
     git: "https://github.com/bozkan/nvd3-sbet.git"
 });
 Package.on_use(function (api) {
